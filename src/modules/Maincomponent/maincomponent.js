b3ddda2b2f8b415371201ded36df9295ce6616ee
ChaudharyShiva18 <shivachaudhary18@outlook.com>
2021-06-02 13:35:25 +0530

    First Commit

---
+++ src/modules/Maincomponent/maincomponent.js
@@ -0,0 +1,21 @@
+import React from "react";
+import styled from "styled-components";
+
+ 
+
+const MainContainer = styled.div`
+  justify-content: center;
+  display: flex;
+`;
+const Container = styled.div`
+  justify-content: center;
+  display: flex;
+`;
+// Xinfin twitter
+export default function MainComponent() {
+  return (
+    <MainContainer>
+      <Container>Xinfintwitter</Container>
+    </MainContainer>
+  );
+}
