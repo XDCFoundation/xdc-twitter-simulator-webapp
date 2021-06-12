721b32e0d2a84cd43da6bffd6acb85b3df1ed6f2
himanshu19hv <himanshu19.gv@gmail.com>
2021-06-12 16:45:48 +0530

    second commit

--- src/routes.js
+++ src/routes.js
@@ -4,7 +4,6 @@ import {Redirect, Switch} from "react-router";
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import {connect} from "react-redux";
-import {Login, SignUp} from "./modules";
 import {history} from "./managers/history";
 import BaseComponent from "./modules/baseComponent";
 import Header from "./modules/Header";
@@ -13,7 +12,6 @@ import Main from "./modules/Maincomponent";
 class Routes extends BaseComponent {
 
     componentDidMount() {
-
     }
 
     render() {
@@ -22,8 +20,8 @@ class Routes extends BaseComponent {
             <MuiThemeProvider muiTheme={getMuiTheme()}>
                 <Router history={history}>
                     <Switch>
-                        <Route exact path={'/'} component={Login}/>
-                        <Route exact path={'/sign-up'} component={SignUp}/>
+                        {/* <Route exact path={'/'} component={Login}/> */}
+                        {/* <Route exact path={'/sign-up'} component={SignUp}/> */}
                         <Route exact path={'/header'} component={Header}/>
                         <Route exact path={'/main'} component={Main}/>
                         <Redirect exact from='*' to="/"/>
