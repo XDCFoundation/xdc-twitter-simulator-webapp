75b85fc609d12f0a43f6eee9cfa7e2899ea9951d
Deepali <deepali@leewayhertz.com>
2021-06-15 19:11:54 +0530

    listing_block

---
+++ src/modules/searchlisting/index.js
@@ -0,0 +1,13 @@
+import BaseComponent from "../baseComponent";
+import React from "react";
+import Searchlist from "./searchListing";
+
+export default class Search extends BaseComponent {
+  render() {
+    return (
+      <>
+        <Searchlist />
+      </>
+    );
+  }
+}
