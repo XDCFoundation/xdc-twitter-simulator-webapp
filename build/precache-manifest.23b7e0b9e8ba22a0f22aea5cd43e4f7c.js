13091ef4a338c5ebb8dbe4db18d8f858c4fa775f
sunnyLeeway <sunnyk@leewayhertz.com>
2021-06-17 12:32:46 +0530

    build

---
+++ build/precache-manifest.23b7e0b9e8ba22a0f22aea5cd43e4f7c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b286eeab2405687344d5be625188d0d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "aff1306778e0525de4b9",
+    "url": "/static/css/2.94cc4e57.chunk.css"
+  },
+  {
+    "revision": "46795bbae5a38011139c",
+    "url": "/static/css/main.36d28d21.chunk.css"
+  },
+  {
+    "revision": "aff1306778e0525de4b9",
+    "url": "/static/js/2.850f3746.chunk.js"
+  },
+  {
+    "revision": "46795bbae5a38011139c",
+    "url": "/static/js/main.8e43c026.chunk.js"
+  },
+  {
+    "revision": "6be70cfec26d1f70986f",
+    "url": "/static/js/runtime~main.557b2043.js"
+  },
+  {
+    "revision": "98c067263b9c69681c5141fe15d5ddb5",
+    "url": "/static/media/Darkmode.98c06726.svg"
+  },
+  {
+    "revision": "407076ad9e2077edb56b297c3f1063d4",
+    "url": "/static/media/light mode.407076ad.svg"
+  }
+]);
