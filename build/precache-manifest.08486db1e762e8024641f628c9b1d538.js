e738d767bf90723d0656be6cc4275ccb5e83db99
Deepali <deepali@leewayhertz.com>
2021-06-30 18:01:56 +0530

    webappsimulatorbuild

---
+++ build/precache-manifest.08486db1e762e8024641f628c9b1d538.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6ae6b35a68764ddb85d35f28bb70908",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3112eade88cdce57bd39",
+    "url": "/static/css/2.94cc4e57.chunk.css"
+  },
+  {
+    "revision": "dc94d26197e7b8a75d9d",
+    "url": "/static/css/main.b6249251.chunk.css"
+  },
+  {
+    "revision": "3112eade88cdce57bd39",
+    "url": "/static/js/2.89640575.chunk.js"
+  },
+  {
+    "revision": "dc94d26197e7b8a75d9d",
+    "url": "/static/js/main.05cf1851.chunk.js"
+  },
+  {
+    "revision": "6be70cfec26d1f70986f",
+    "url": "/static/js/runtime~main.557b2043.js"
+  },
+  {
+    "revision": "e7b17cfc5a2de3e3d165fed30e5cbef8",
+    "url": "/static/media/Darkmode.e7b17cfc.svg"
+  },
+  {
+    "revision": "8782639dd1c8a15a3bdbf9b117c27a9c",
+    "url": "/static/media/light mode.8782639d.svg"
+  }
+]);
