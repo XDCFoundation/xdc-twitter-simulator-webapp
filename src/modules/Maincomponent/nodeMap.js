dfd35acd2edf3fd15889763ab198a3eebb1ad2e1
Deepali <deepali@leewayhertz.com>
2021-06-30 10:46:08 +0530

    map_integration

---
+++ src/modules/Maincomponent/nodeMap.js
@@ -0,0 +1,31 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import {
+  ComposableMap,
+  Geographies,
+  Geography,
+  Marker,
+} from "react-simple-maps";
+
+const geoUrl =
+  "https://raw.githubusercontent.com/zcreativelabs/react-simple-maps/master/topojson-maps/world-110m.json";
+
+const NodeChart = () => {
+  return (
+    <ComposableMap
+      style={{
+        fill: "#bdc1e5",
+      }}
+    >
+      <Geographies geography={geoUrl}>
+        {({ geographies }) =>
+          geographies.map((geo) => (
+            <Geography key={geo.rsmKey} geography={geo} />
+          ))
+        }
+      </Geographies>
+    </ComposableMap>
+  );
+};
+
+export default NodeChart;
