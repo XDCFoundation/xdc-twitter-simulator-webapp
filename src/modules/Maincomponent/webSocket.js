9a0b4c66bb643717c2389c37a4666bb54213461f
himanshuleewayhertz <himanshub@leewayhertz.com>
2021-07-09 12:47:13 +0530

    bug resolved

--- src/modules/Maincomponent/webSocket.js
+++ src/modules/Maincomponent/webSocket.js
@@ -4,7 +4,7 @@ const client = new W3CWebSocket("wss://stats1.apothem.network/primus/?_primuscb=
 export default class WebSocketCountNode extends Component {
     constructor(props) {
         super(props);
-        this.state = { value1: 0 };
+        this.state = { value: 0 };
     }
     componentDidMount() {
         this.getValue()
@@ -14,21 +14,22 @@ export default class WebSocketCountNode extends Component {
         client.onopen = () => {
             console.log("connect")
         }
-        client.onmessage = function (event) {
+        client.onmessage = async (event)=> {
             var msg = JSON.parse(event.data)
             if (msg.action === 'stats') {
                 if (msg.data.id in test) {
                     return
                 } else {
-                    console.log('Updated test data===', Object.keys(test).length)
+                    // console.log('Updated test data===', Object.keys(test).length)
                     test[msg.data.id] = msg.data.stats.active
+                    await this.setState({ value: Object.keys(test).length })
                 }
             }
         }
         client.onclose = async (event) => {
             if (event.wasClean) {
                 console.log(`Number of Active Nodes = ${Object.keys(test).length}`)
-                await this.setState({ value: Object.keys(test).length })
+                // await this.setState({ value: Object.keys(test).length })
             } else {
                 console.log('[close] Connection died')
             }
@@ -37,7 +38,7 @@ export default class WebSocketCountNode extends Component {
 
     render() {
         return (
-            <div>{this.state.value?this.state.value:"....Loading number of Nodes"}</div>
+            <div>{this.state.value}</div>
         )
     }
 }
