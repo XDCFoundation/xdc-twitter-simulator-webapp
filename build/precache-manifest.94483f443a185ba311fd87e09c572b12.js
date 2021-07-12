23a5994d5ccffcbd71b6b2db11d2d25b7bbf58e2
PankajSingh02 <pankajs@leewayhertz.com>
2021-07-12 21:33:45 +0530

    build

---
+++ build/precache-manifest.94483f443a185ba311fd87e09c572b12.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "79c5621f751c1c2b40eb27f97e140cd7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c6f53a0630ee0937702b",
+    "url": "/static/css/2.08e7a487.chunk.css"
+  },
+  {
+    "revision": "7f650f52f34882d68b74",
+    "url": "/static/css/main.fec6262f.chunk.css"
+  },
+  {
+    "revision": "c6f53a0630ee0937702b",
+    "url": "/static/js/2.9b16587e.chunk.js"
+  },
+  {
+    "revision": "7f650f52f34882d68b74",
+    "url": "/static/js/main.b74cb3fd.chunk.js"
+  },
+  {
+    "revision": "6be70cfec26d1f70986f",
+    "url": "/static/js/runtime~main.557b2043.js"
+  },
+  {
+    "revision": "7704399918c62221ba916353806fb070",
+    "url": "/static/media/Darkmode.77043999.svg"
+  },
+  {
+    "revision": "6afe2aba19ea1864d7e8673dbc182129",
+    "url": "/static/media/light mode.6afe2aba.svg"
+  }
+]);
