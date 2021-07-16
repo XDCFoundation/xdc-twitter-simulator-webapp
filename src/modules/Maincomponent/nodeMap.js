0a62dd4245a60a5acf54edc1576f3c0f267924b2
Mohd Kashif <kashif@leewayhertz.com>
2021-07-16 13:05:45 +0530

    node map

--- src/modules/Maincomponent/nodeMap.js
+++ src/modules/Maincomponent/nodeMap.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 import {
   ComposableMap,
@@ -6,59 +7,69 @@ import {
   Geography,
   Marker,
 } from "react-simple-maps";
-const markers = [
-  {
-    markerOffset: -30,
-    coordinates: [-5.3816, -34.6037]
-  },
-  { markerOffset: 15, coordinates: [78.9629, 20.5937] },
-  { markerOffset: 15, coordinates: [133.7751, -25.2744] },
-  { markerOffset: 15, coordinates: [-102.5528, 23.6345] },
-  { markerOffset: 15, coordinates: [-3.7492, 40.4637] },
-  { markerOffset: 15, coordinates: [3.406448, 6.465422] },
-  { markerOffset: -30, coordinates: [45.0792, 23.8859] },
-  { markerOffset: -30, coordinates: [-57.5759, -25.2637] },
-  { markerOffset: 15, coordinates: [-55.2038, 5.852] },
-];
-
 
 const geoUrl =
   "https://raw.githubusercontent.com/zcreativelabs/react-simple-maps/master/topojson-maps/world-110m.json";
 
-
 const NodeChart = (props) => {
+  const [node, setNode] = useState([]);
+  useEffect(() => {
+    noOfNodes();
+    setInterval(() => {
+      noOfNodes();
+    }, 10000);
+  }, []);
+  const noOfNodes = () => {
+    axios
+      .get("https://ki3l56sayb.execute-api.us-east-2.amazonaws.com/nodeNumber")
+      .then((res) => {
+        // alert(JSON.stringify(res));
+        setNode(res.data);
+        console.log("responseDataChekk", res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
 
   return (
     <ComposableMap
-      style={props.dark ? {
-        fill: "#293178"
-      } : { fill: "#bdc1e5" }}
+      style={
+        props.dark
+          ? {
+              fill: "#293178",
+            }
+          : { fill: "#bdc1e5" }
+      }
     >
       <Geographies geography={geoUrl}>
         {({ geographies }) =>
           geographies.map((geo) => (
-            <Geography key={geo.rsmKey} geography={geo} >
-
-            </Geography>
+            <Geography key={geo.rsmKey} geography={geo}></Geography>
           ))
         }
       </Geographies>
-      {markers.map(({ name, coordinates, markerOffset }) => (
-        <Marker key={name} coordinates={coordinates}>
-          <g
-            fill="#10c892"
-            stroke="#10c892"
-            strokeWidth="2"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            transform="translate(-12, -24)"
-          >
-            <circle cx="12" cy="" r="9" />
-          </g>
-        </Marker>
-      ))
-      }
 
+      {/* {markers.map(({ name, coordinates, markerOffset }) => { */}
+
+      {node.slice(1).map((response) => {
+        let newValue = response.coordinates;
+        // console.log("coordinates",newValue)
+        return (
+          <Marker coordinates={newValue}>
+            <g
+              fill="#10c892"
+              stroke="#10c892"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              transform="translate(-12, -24)"
+            >
+              <circle cx="12" cy="" r="9"></circle>
+            </g>
+          </Marker>
+        );
+      })}
     </ComposableMap>
   );
 };
