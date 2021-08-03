72b3ef7df38c0939a816c346fdf9f2b9df0ed4be
lwhz-nitish <nitish@leewayhertz.com>
2021-08-03 17:55:03 +0530

    build updated

---
+++ build/precache-manifest.aa057a73448804f07d824ea1e5d66f04.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77a819ddddd9b59f233f6346018f6cd4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5d857dd3512a90ddaf2f",
+    "url": "/static/css/2.275d5f68.chunk.css"
+  },
+  {
+    "revision": "369c64913b14d170d7a0",
+    "url": "/static/css/main.e252d2ff.chunk.css"
+  },
+  {
+    "revision": "5d857dd3512a90ddaf2f",
+    "url": "/static/js/2.e79836d5.chunk.js"
+  },
+  {
+    "revision": "369c64913b14d170d7a0",
+    "url": "/static/js/main.9a39bb0d.chunk.js"
+  },
+  {
+    "revision": "6be70cfec26d1f70986f",
+    "url": "/static/js/runtime~main.557b2043.js"
+  },
+  {
+    "revision": "7704399918c62221ba916353806fb070",
+    "url": "/static/media/Darkmode.77043999.svg"
+  },
+  {
+    "revision": "6afe2aba19ea1864d7e8673dbc182129",
+    "url": "/static/media/light mode.6afe2aba.svg"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.9c83e721ab335fc8ca06517c690fa984.js"
+  "/precache-manifest.aa057a73448804f07d824ea1e5d66f04.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
