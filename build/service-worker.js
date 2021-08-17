617532491639af746c4e968d2e2498bcc65e06e0
Abhey gupta <abheyygupta396@gmail.com>
2021-08-17 18:27:17 +0530

    webapp-build

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.ecbdf3fdefe67e17a6ea20486afe2ecf.js"
+  "/precache-manifest.6bec48148b9b129ae14572575f90916c.js"
 );
 
 self.addEventListener('message', (event) => {
