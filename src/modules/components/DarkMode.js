fb71b9251799d3c225031aa94452423c1d801bf8
Abhey gupta <abheyygupta396@gmail.com>
2021-08-18 17:17:41 +0530

    Integrated the save tweets count response from its api and fix some issues

--- src/modules/components/DarkMode.js
+++ src/modules/components/DarkMode.js
@@ -24,13 +24,13 @@ const DarkMode = (props) => {
       e.target.classList.remove(clickedClass);
       localStorage.setItem("theme", "light");
       theme = lightTheme;
-      props.CheckMode(false)
+      props.CheckMode(true)
     } else {
       body.classList.replace(lightTheme, darkTheme);
       e.target.classList.add(clickedClass);
       localStorage.setItem("theme", "dark");
       theme = darkTheme;
-      props.CheckMode(true)
+      props.CheckMode(false)
     }
   };
 
