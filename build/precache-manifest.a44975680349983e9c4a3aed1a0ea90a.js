88e465c4cbad58b6ab8a903c2544d630a2260105
jyot <jyotk@leewayhertz.com>
2021-08-18 18:40:51 +0530

    master build

---
+++ build/precache-manifest.a44975680349983e9c4a3aed1a0ea90a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7872c6db04b1a66ae90c9b22267d4909",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e84b3310fb5360fd2f97",
+    "url": "/static/css/2.ed5b0c13.chunk.css"
+  },
+  {
+    "revision": "f4b40bbe4a34b0422ec7",
+    "url": "/static/css/main.65e38593.chunk.css"
+  },
+  {
+    "revision": "e84b3310fb5360fd2f97",
+    "url": "/static/js/2.9796177e.chunk.js"
+  },
+  {
+    "revision": "f4b40bbe4a34b0422ec7",
+    "url": "/static/js/main.2116e87d.chunk.js"
+  },
+  {
+    "revision": "6be70cfec26d1f70986f",
+    "url": "/static/js/runtime~main.557b2043.js"
+  },
+  {
+    "revision": "378206c8ec1e203d65178ebde312a313",
+    "url": "/static/media/Darkmode.378206c8.svg"
+  },
+  {
+    "revision": "2294f5b62d4b8b0daf2859fd0b52e7a0",
+    "url": "/static/media/light mode.2294f5b6.svg"
+  }
+]);
