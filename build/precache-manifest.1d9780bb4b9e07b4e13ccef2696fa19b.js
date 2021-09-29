b7944ba5ce13feb1b0e6cf6c7f7407dcc4ffd1c0
Abhey gupta <abheyygupta396@gmail.com>
2021-09-29 19:29:04 +0530

    build

---
+++ build/precache-manifest.1d9780bb4b9e07b4e13ccef2696fa19b.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40a7ce26c50b1b56370627d12f82fc9b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "93bebf653a4b155d55cd",
+    "url": "/static/css/2.94b3272b.chunk.css"
+  },
+  {
+    "revision": "2564342132dfce6e816a",
+    "url": "/static/css/main.add3b578.chunk.css"
+  },
+  {
+    "revision": "93bebf653a4b155d55cd",
+    "url": "/static/js/2.fc64eac2.chunk.js"
+  },
+  {
+    "revision": "2564342132dfce6e816a",
+    "url": "/static/js/main.8ab39758.chunk.js"
+  },
+  {
+    "revision": "6be70cfec26d1f70986f",
+    "url": "/static/js/runtime~main.557b2043.js"
+  },
+  {
+    "revision": "5b1971bf0732cb6e04291733c1c35296",
+    "url": "/static/media/Banner.5b1971bf.png"
+  },
+  {
+    "revision": "7704399918c62221ba916353806fb070",
+    "url": "/static/media/Darkmode.77043999.svg"
+  },
+  {
+    "revision": "f643c39631857c7202cfacc34e5f9965",
+    "url": "/static/media/MainHeader.f643c396.png"
+  },
+  {
+    "revision": "b1acc9896360ad9082ebe1ac662c1522",
+    "url": "/static/media/Webshot.b1acc989.png"
+  },
+  {
+    "revision": "6afe2aba19ea1864d7e8673dbc182129",
+    "url": "/static/media/light mode.6afe2aba.svg"
+  }
+]);
