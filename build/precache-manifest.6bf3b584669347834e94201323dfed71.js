fedd5246ab1df1ccc18248abfd5beebdef40121a
Abhey_AbheyGupta <abhey@leewayhertz.com>
2022-02-14 16:56:44 +0530

    production build

---
+++ build/precache-manifest.6bf3b584669347834e94201323dfed71.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8686071a4726f4c7066b4a7b40611997",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6ae236173522001b3876",
+    "url": "/static/css/2.65cb6532.chunk.css"
+  },
+  {
+    "revision": "b96844804e7dacad16bc",
+    "url": "/static/css/main.dcb894cd.chunk.css"
+  },
+  {
+    "revision": "6ae236173522001b3876",
+    "url": "/static/js/2.02c5cda3.chunk.js"
+  },
+  {
+    "revision": "b96844804e7dacad16bc",
+    "url": "/static/js/main.cd24c878.chunk.js"
+  },
+  {
+    "revision": "6be70cfec26d1f70986f",
+    "url": "/static/js/runtime~main.557b2043.js"
+  },
+  {
+    "revision": "5b1971bf0732cb6e04291733c1c35296",
+    "url": "/static/media/Banner.5b1971bf.png"
+  },
+  {
+    "revision": "7704399918c62221ba916353806fb070",
+    "url": "/static/media/Darkmode.77043999.svg"
+  },
+  {
+    "revision": "f643c39631857c7202cfacc34e5f9965",
+    "url": "/static/media/MainHeader.f643c396.png"
+  },
+  {
+    "revision": "b1acc9896360ad9082ebe1ac662c1522",
+    "url": "/static/media/Webshot.b1acc989.png"
+  },
+  {
+    "revision": "6afe2aba19ea1864d7e8673dbc182129",
+    "url": "/static/media/light mode.6afe2aba.svg"
+  }
+]);
