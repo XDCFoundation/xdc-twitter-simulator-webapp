9a72c3719db0391c8b1f8ed5c786404580cc165b
Abhey_AbheyGupta <abhey@leewayhertz.com>
2022-03-09 15:48:38 +0530

    production build

---
+++ build/precache-manifest.4ed16157677de1b956e60953f8338af0.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e43218989e0c387ec1762fb939971b6d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "641cc02f7879ef0eb8be",
+    "url": "/static/css/0.abf88443.chunk.css"
+  },
+  {
+    "revision": "1765dcd45c7aadf63020",
+    "url": "/static/css/3.a95331b8.chunk.css"
+  },
+  {
+    "revision": "22795f24ff598f48ac72",
+    "url": "/static/css/main.83b47e21.chunk.css"
+  },
+  {
+    "revision": "641cc02f7879ef0eb8be",
+    "url": "/static/js/0.ec378bec.chunk.js"
+  },
+  {
+    "revision": "1765dcd45c7aadf63020",
+    "url": "/static/js/3.462308a3.chunk.js"
+  },
+  {
+    "revision": "cb51e9263b8d8f58ff76",
+    "url": "/static/js/4.0fac1cf8.chunk.js"
+  },
+  {
+    "revision": "6c3e6e5baa4cf7b68e97",
+    "url": "/static/js/5.82ed5869.chunk.js"
+  },
+  {
+    "revision": "55a4583626076b2cdfa9",
+    "url": "/static/js/6.2f89b2f1.chunk.js"
+  },
+  {
+    "revision": "39686f156df441133327",
+    "url": "/static/js/7.9935cfed.chunk.js"
+  },
+  {
+    "revision": "177690cdf0cae43c8678",
+    "url": "/static/js/8.891b5388.chunk.js"
+  },
+  {
+    "revision": "22795f24ff598f48ac72",
+    "url": "/static/js/main.e11dbd30.chunk.js"
+  },
+  {
+    "revision": "389c8a03d77399b9c143",
+    "url": "/static/js/runtime~main.a6baa9d5.js"
+  },
+  {
+    "revision": "5b1971bf0732cb6e04291733c1c35296",
+    "url": "/static/media/Banner.5b1971bf.png"
+  },
+  {
+    "revision": "7704399918c62221ba916353806fb070",
+    "url": "/static/media/Darkmode.77043999.svg"
+  },
+  {
+    "revision": "f643c39631857c7202cfacc34e5f9965",
+    "url": "/static/media/MainHeader.f643c396.png"
+  },
+  {
+    "revision": "b1acc9896360ad9082ebe1ac662c1522",
+    "url": "/static/media/Webshot.b1acc989.png"
+  },
+  {
+    "revision": "6afe2aba19ea1864d7e8673dbc182129",
+    "url": "/static/media/light mode.6afe2aba.svg"
+  }
+]);
