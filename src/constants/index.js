71143f909087c003af36cd2f2184600990ee4513
samsheerk <samsheer@leewayhertz.com>
2022-04-01 15:19:07 +0530

    intregate  save max TPS count commit

--- src/constants/index.js
+++ src/constants/index.js
@@ -19,7 +19,10 @@ export const httpConstants = {
     DEVICE_TYPE: {
         WEB: 'web'
     },
-    API_END_POINT: {}
+    API_END_POINT: {
+        SAVE_MAX_TPS: '/save-max-tps-count'
+    }
+
 };
 
 export const socialMediaLinks = {
